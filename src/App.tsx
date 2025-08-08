763095e0d920b119545817792a01d3d8a6bdac88
kun224 <skyday0330@gmail.com>
2025-08-08 22:52:54 +0900

    feat: Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
